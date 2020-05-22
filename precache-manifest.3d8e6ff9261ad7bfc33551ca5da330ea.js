d332f4870a67f3b026b61a8ba7facd545c706c07
Ursula Kesek <ursky@blueyonder.co.uk>
2020-05-22 11:18:13 +0100

    Updates

---
+++ precache-manifest.3d8e6ff9261ad7bfc33551ca5da330ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858eabb5e50ca0c0444d4b33d6bdc2ae",
+    "url": "/registration/index.html"
+  },
+  {
+    "revision": "c37bd851acbf8cd0807e",
+    "url": "/registration/static/css/main.28cb7246.chunk.css"
+  },
+  {
+    "revision": "0eef7a2dd9f90b1d72f8",
+    "url": "/registration/static/js/2.b58fb8ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/registration/static/js/2.b58fb8ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c37bd851acbf8cd0807e",
+    "url": "/registration/static/js/main.4b557684.chunk.js"
+  },
+  {
+    "revision": "a84f8df832a6e92cce8a",
+    "url": "/registration/static/js/runtime-main.86956daf.js"
+  }
+]);
